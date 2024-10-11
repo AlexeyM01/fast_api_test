07354a6585c3e197a7a00eeabd566f3db5616705
Alexey Mochalov <AlexeyM01@yandex.ru>
2024-10-11 17:24:52 +0300

    Added basic html templates

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from auth.base_config import auth_backend, fastapi_users
 from auth.schemas import UserCreate, UserRead
 from operations.router import router as router_operation
 from tasks.router import router as router_tasks
+from pages.router import router as router_pages
 
 app = FastAPI(
     title="Trading App"
@@ -26,6 +28,25 @@ app.include_router(
 
 app.include_router(router_operation)
 app.include_router(router_tasks)
+app.include_router(router_pages)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+origins = [#доступ к бэкэнду
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,#куки
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 
 @app.on_event("startup")
